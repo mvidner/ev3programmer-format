3d754168d13676b663d1f0ade1fc8940e4a38b58
Martin Vidner <martin@vidner.net>
2017-05-04 15:49:50 +0200

    Handle filenames with whitespace

--- ev3j2ev3j.rb
+++ ev3j2ev3j.rb
@@ -8,6 +8,6 @@ ARGV.each do |base|
   base = base.sub(/\.ev3j$/, "")
   r = Ev3j::RobotProgram.from_json_file(base + ".ev3j")
   r.save_json(base + ".out.ev3j")
-  system "python -m json.tool #{base}.out.ev3j > #{base}.out.pp.ev3j"
-  system "diff -u #{base}.pp.ev3j #{base}.out.pp.ev3j"
+  system "python -m json.tool '#{base}.out.ev3j' > '#{base}.out.pp.ev3j'"
+  system "diff -u '#{base}.pp.ev3j' '#{base}.out.pp.ev3j'"
 end
