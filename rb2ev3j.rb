80df75dd202e45e11c93ff23dbbd0aa7e8034f13
Martin Vidner <martin@vidner.net>
2017-05-04 14:55:02 +0200

    Fixed the invocation, diff the result

--- rb2ev3j.rb
+++ rb2ev3j.rb
@@ -2,11 +2,11 @@
 $: << "lib"
 require "robot"
 
-
 ARGV.each do |base|
-  base.sub!(/\.rb$/, "")
+  base = base.sub(/\.rb$/, "")
   r = Ev3j::Robot.new
   r.script_file(base + ".rb")
   r.save_json(base + ".out.ev3j")
   system "python -m json.tool #{base}.out.ev3j > #{base}.out.pp.ev3j"
+  system "diff -u #{base}.pp.ev3j #{base}.out.pp.ev3j"
 end
